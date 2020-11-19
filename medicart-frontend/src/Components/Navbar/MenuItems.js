49020a94def34aab44cd9389460ea1806144c1ee
Femi Akinyemi <akinfemi46@gmail.com>
2020-11-19 18:02:12 +0100

    Added Navbar

--- medicart-frontend/src/Components/Navbar/MenuItems.js
+++ medicart-frontend/src/Components/Navbar/MenuItems.js
@@ -1,27 +1,28 @@
 export const MenuItems = [
-    {
-        title:'Home',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title:'Services',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title:'Products',
-        url: '#',
-        cName: 'nav-links'
-    },
+    // {
+    //     title:'Home',
+    //     url: '#',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title:'Services',
+    //     url: '#',
+    //     cName: 'nav-links'
+    // },
+    // {
+    //     title:'Products',
+    //     url: '#',
+    //     cName: 'nav-links'
+    // },
     {
         title:'Contact Us',
         url: '#',
-        cName: 'nav-links'
+        cName: 'nav-links '
     },
+    
     {
-        title:'Sign up',
-        url: '#',
+        title: <i className="fas fa-cart-plus"></i>,
+        Url: '#',
         cName: 'nav-links'
-    },
+    }
 ]
--- medicart-frontend/src/Components/Navbar/Navbar.jsx
+++ medicart-frontend/src/Components/Navbar/Navbar.jsx
@@ -14,13 +14,17 @@ handleClick =()=>{
   render() {
     return (
       <div className="NavbarItems">
-        <h1 className="navbar-logo">React <i className="fab fa-react"></i>  </h1>
+        <h1 className="navbar-logo">MEDIC<i className="fas fa-cart-plus"></i>RT   </h1>
         <div className="menu-icon" onClick={this.handleClick}>
-<i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
         <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+        <button className="contact">Register</button>
           {MenuItems.map((item, index) => {
+              <Button>Sign Up</Button>
+              
             return (
+                
               <li>
                 <a className={item.cName} href={item.url}>
                   {item.title}
@@ -28,8 +32,9 @@ handleClick =()=>{
               </li>
             );
           })}
+          
         </ul>
-        <Button>Sign Up</Button>
+        
       </div>
     );
   }
