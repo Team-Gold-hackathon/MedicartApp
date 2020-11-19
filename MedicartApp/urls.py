5b4c23719e5e4aaa360be3ff18fd0c92e8eab750
temitayo <47720307+temitayopelumi@users.noreply.github.com>
2020-11-19 21:47:19 +0100

    Add files via upload

--- MedicartApp/urls.py
+++ MedicartApp/urls.py
@@ -4,5 +4,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     #path('', TemplateView.as_view(template_name='index.html')), 
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    # path('account/', views.accountSettings, name="account"),
+
+    path('products/', views.products, name="products"),
 ]
--- MedicartApp/views.py
+++ MedicartApp/views.py
@@ -1,15 +1,57 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
+from .forms  import OrderForm, CreateUserForm, CustomerForm
+from django.forms import inlineformset_factory
 # Create your views here.
+#from .filters import OrderFilter
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user,allowed_users,admin_only
+from django.contrib.auth.models import Group
+
 
 def home(request):
     return render(request, 'index.html')
 
-# @login_required(login_url='login')
-# @allowed_users(allowed_roles=['admin'])
-# def products(request):
-#     products=Product.objects.all()
-#     return render(request, 'check/products.html',{'products':products})
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username =  form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for'+username)
+            return redirect('home')
+    
+    context = {'form':form}
+    return render(request, "signup.html", context)
+    
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'User or Password is incorrect')
+    context = {}
+    return render(request, "login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin', 'pharmacy'])
+def products(request):
+    products=Product.objects.all()
+    return render(request, 'products.html',{'products':products})
 
 # @login_required(login_url='login')
 # @allowed_users(allowed_roles=['admin'])
