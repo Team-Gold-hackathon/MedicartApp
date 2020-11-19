d000b63d478ce7c17e2a43d54d4ca61ff4cd360f
Femi Akinyemi <akinfemi46@gmail.com>
2020-11-19 16:43:43 +0100

    deleted gitignore

--- medicart-frontend/src/Components/Navbar/Navbar.jsx
+++ medicart-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Button } from "./Button";
+import { MenuItems } from "./MenuItems";
 
-function Navbar() {
-  return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
-  )
+export default class Navbar extends Component {
+
+state = {
+    clicked: false
+}
+handleClick =()=>{
+    this.setState({clicked: !this.state.clicked})
 }
 
-export default Navbar
+  render() {
+    return (
+      <div className="NavbarItems">
+        <h1 className="navbar-logo">React <i className="fab fa-react"></i>  </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+<i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Sign Up</Button>
+      </div>
+    );
+  }
+}
